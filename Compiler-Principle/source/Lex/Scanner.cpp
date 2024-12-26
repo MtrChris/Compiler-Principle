c0360625af2d5bccbfaf2d0489efcdad9bf766e3
123-123-1 <3182821816@qq.com>
2024-12-27 02:20:17 +0800

    Revert "修复乱码"
    This reverts commit ac9226b7c9cb7564a7b0745ad69177d1b8539772.

--- Compiler-Principle/source/Lex/Scanner.cpp
+++ Compiler-Principle/source/Lex/Scanner.cpp
@@ -1,18 +1,20 @@
-﻿#include "Lex.h"
+#include "Lex.h"
 #include <fstream>
 #include <sstream>
 using namespace std;
 
 extern bitset<MAXCH>recognizeCh;
-
+// ���ֱ� 
 extern vector<NametabItem> nametab;
 extern vector<pair<Automaton*, map<int, string>>>DFAS;
+// �洢�����ļ� 
 extern vector<string> codes;
 
-int nameIndex = 0;	
-int currPos = 0;	
-int lineCount = 0;	
- 
+int nameIndex = 0;	// ������ţ���0��ʼ�� 
+int currPos = 0;	// ��ǰ�ַ��±꼴��������0��ʼ�� 
+int lineCount = 0;	// ���� ����0��ʼ��
+
+// �����ֱ������Ƿ�����name���ҵ������±꣬���򷵻�-1 
 int findName(string name) {
 	for (int i = 0; i < nametab.size(); i++) {
 		if (nametab[i].name == name) {
@@ -22,9 +24,13 @@ int findName(string name) {
 	return -1;
 }
 
-
-int readnext(NametabItem& item, Automaton* dfa, map<int, string>& finalStates, LexException error) {
-	string newName = ""; 
+// ÿ����һ��ʶ��һ���´�
+// item: NametabItem --> ʶ�𵽵Ĵ� 
+// ����ֵ: SYN_ERROR --> �﷨���� 
+//		   NOT_FINISHED --> ��ȡδ��� 
+//		   FINISHED --> ��ȡ��� 
+int readnext(NametabItem& item,Automaton* dfa,map<int,string>& finalStates,string& error) {
+	string newName = ""; // �г����Ĵ� 
 	//printNFA(dfa);
 	int beginState = dfa->begin;
 	int nowState = beginState;
@@ -38,14 +44,14 @@ int readnext(NametabItem& item, Automaton* dfa, map<int, string>& finalStates, L
 		return FINISHED;
 	}
 	else {
-		throw LexException("读取超过文件范围！");
+		throw LexException("��ȡ�����ļ���Χ��");
 	}
-
+	
 	int lineLength = codes[lineCount].length();
 	while (currPos <= lineLength) {
-		char ch = line[currPos]; 
-		arrvState = getEndState(dfa, nowState, ch);	
-
+		char ch = line[currPos];	// ���һ���ַ�line[lineLength]��'\0' 
+		arrvState = getEndState(dfa, nowState, ch);	// ����Զ����Ƿ��ܽ�������ַ� 
+		// ���ܽ��� 
 		if (arrvState == -1) {
 			if (nowState == dfa->begin) {
 				if (ch == ' ') {
@@ -54,7 +60,7 @@ int readnext(NametabItem& item, Automaton* dfa, map<int, string>& finalStates, L
 				}
 				else if (!recognizeCh.test(ch)) {
 					newName += ch;
-					NametabItem nametabItem(-1, newName, newName, 0);
+					NametabItem nametabItem(-1, newName,newName, 0);
 					item = nametabItem;
 					currPos++;
 					if (currPos == lineLength) {
@@ -65,56 +71,68 @@ int readnext(NametabItem& item, Automaton* dfa, map<int, string>& finalStates, L
 			}
 			auto it_fs = finalStates.find(nowState);
 			if (it_fs != finalStates.end()) {
+				// ��ǰ������̬ 
 				if (currPos == lineLength) {
+					// ���ж����ˣ�����һ�� 
 					lineCount++, currPos = 0;
 				}
 
-				int res = findName(newName);	
+				int res = findName(newName);	// ����Ƿ����´� 
 				if (res == -1) {
 					NametabItem nametabItem(nameIndex++, newName, it_fs->second, 0);
 					item = nametabItem;
-					return NOT_FINISHED;	
+					return NOT_FINISHED;	// ��δ���� 
 				}
 				else {
 
 					item = nametab[res];
-					return NOT_FINISHED;	
+					return NOT_FINISHED;	// ��δ���� 
 				}
 			}
 			else {
 				ostringstream oss;
+				// ��ǰû����̬������ 
 				if (line[currPos] == '\0') {
-				    error= LexException(FORMATEXCEPTION(lineCount + 1, currPos + 1, codes[lineCount], "缺少字符"));
+					oss << "��" << lineCount + 1 << "�е�" << currPos + 1 << "�У�"
+						<< codes[lineCount] << " "
+						<< "�﷨����ȱ���ַ���";
+					error = oss.str();
 				}
 				else {
-					error= LexException(FORMATEXCEPTION(lineCount + 1, currPos + 1, codes[lineCount], line[currPos]));
+					oss << "��" << lineCount + 1 << "�е�" << currPos + 1 << "�У�"
+						<< codes[lineCount] << " "
+						<< "�﷨����\"" << line[currPos] << "\"" << endl;
+					error = oss.str();
 				}
-
-				return SYN_ERROR;	
+				
+				return SYN_ERROR;	// �˳��������﷨���� 
 			}
 		}
 		else {
+			// ���Խ��� 
 			newName += ch;
 			nowState = arrvState;
 			currPos++;
 		}
 	}
 
-	return FINISHED;	
+	return FINISHED;	// �����ļ���ȡ��ϣ�lineCount == codes.size() 
 }
 
+// �Ӵ����ļ��ж�ȡ���� 
 void readCode(string path) {
 	ifstream file(path);
 	if (!file.is_open()) {
-		throw ios_base::failure("代码文件读取失败");
+		throw ios_base::failure("�����ļ���ȡʧ��");
 	}
 
 	string line;
 	while (getline(file, line)) {
 		codes.push_back(line);
-		cout << line << endl;
+		cout << line<<endl;
 	}
-
+	
+	// �ر��ļ�
 	file.close();
 }
 
@@ -126,10 +144,10 @@ struct state {
 	int r;
 };
 
-int readNext(NametabItem& item)
+int readNext(NametabItem& item) 
 {
 
-	LexException e;
+	string e;
 	NametabItem a;
 	vector<state>p;
 	p.push_back(state{ nameIndex, currPos, lineCount,NametabItem(), 0 });
@@ -138,15 +156,15 @@ int readNext(NametabItem& item)
 		currPos = p[0].b;
 		lineCount = p[0].c;
 		int k = 0;
-		k = readnext(a, i.first, i.second, e);
-		if (k == NOT_FINISHED) {
+		k= readnext(a, i.first, i.second,e);
+		if (k == NOT_FINISHED ) {
 			p.push_back(state{ nameIndex, currPos, lineCount,a, k });
 		}
-
+		
 	}
-
-	if (p.size() == 1) {
-		throw e;
+	
+	if (p.size()==1) {
+		cout << e;
 		return SYN_ERROR;
 	}
 	auto it = max_element(p.begin(), p.end(), [](state a, state b) {
@@ -161,6 +179,6 @@ int readNext(NametabItem& item)
 	nameIndex = it->a;
 	currPos = it->b;
 	lineCount = it->c;
-	nametab.push_back(item);
+	nametab.push_back(item);	
 	return it->r;
 }
